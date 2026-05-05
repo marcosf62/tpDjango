ae0a6763de801a9188fb1402ffcb0a122e110afb
marcosf62 <m.funes@itecriocuarto.org.ar>
2026-05-05 20:58:52 -0300

    agrego decorador

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Cursos
-from .models import Prof
+from .models import Cursos, Prof
 
-admin.site.register(Cursos)
 admin.site.register(Prof)
+
+@admin.register(Cursos)
+class TareaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre","nivel","profe")
+    list_filter = ("nombre","profe")
+    search_fields = ("nombre",)
